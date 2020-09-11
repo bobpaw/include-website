34e23e30c79592fb924f6a1f0ad9f6d7e9beb7b1
Adison Lampert <38566184+addybug@users.noreply.github.com>
2020-09-11 12:49:07 -0500

    added route to week 2

--- website/server.js
+++ website/server.js
@@ -151,3 +151,14 @@ app.use(protectPath(/^\/protected\/.*$/));
 	 }
 	 response.end();
  });
+
+ app.get('/web-development/week-2', function(request, response) {
+  if (request.session.loggedin) {
+  let html = fs.readFileSync(path.join(__dirname,'/protected/week2.html'));
+   response.writeHead(200, {'Content-Type': 'text/html'});
+   response.end(html);
+  } else {
+    return response.send('Please login to view this page!');
+  }
+  response.end();
+ });
